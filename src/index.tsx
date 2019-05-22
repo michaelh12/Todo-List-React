e682f6d92e6fb4d5325cbe23237c7216b4d1ae88
Michael Habinsky <mhabinsky@live.com>
2019-05-22 16:00:22 -0400

    minor css styling updates

--- src/index.tsx
+++ src/index.tsx
@@ -54,31 +54,36 @@ export default function App(): JSX.Element {
     }
 
     return (
-        <Fragment>
-            <div className="container">
-                <h1>Todo List</h1>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        value={value}
-                        onChange={e => setValue(e.target.value)}
-                        required />
-                    <button type="submit">Add Todo</button>
-                </form>
+        <Fragment >
+
+            <div id="main-container">
+                <div className="container">
+                    <h1>Todo List</h1>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                            required />
+                        <button type="submit" id="submit">Add Todo</button>
+                    </form>
+                </div>
+
+                <section>
+                    {todos.map((todo: ITodo, index: number) =>
+                        <Fragment key={index} >
+                            <div style={{ textDecoration: todo.complete ? 'line-through' : '' }}>{todo.text} ping </div>
+                            <button type="button" onClick={() => completeTodo(index)} >
+                                {' '}
+                                {todo.complete ? 'Incomplete' : 'Complete'}
+                            </button>
+                            <button type="button" onClick={() => { deleteTodo(index) }}>&times;</button>
+                        </Fragment>
+                    )}
+
+                </section>
             </div>
 
-            <section>
-                {todos.map((todo: ITodo, index: number) =>
-                    <Fragment key={index} >
-                        <div style={{ textDecoration: todo.complete ? 'line-through' : '' }}>{todo.text} ping </div>
-                        <button type="button" onClick={() => completeTodo(index)}>
-                            {' '}
-                            {todo.complete ? 'Incomplete' : 'Complete'}
-                        </button>
-                        <button type="button" onClick={() => { deleteTodo(index) }}>&times;</button>
-                    </Fragment>
-                )}
-            </section>
         </Fragment >
     )
 }
